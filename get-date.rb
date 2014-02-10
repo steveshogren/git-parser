413ea46f58f882a7433a47ae94c6b9ef0d1238e8
Steve Shogren <steve.a.shogren@gmail.com>
2014-02-09 19:13:17 -0500

    Fixing the parser to find the next date after a given repo has been worked on

--- get-date.rb
+++ get-date.rb
@@ -1,9 +1,24 @@
-places = Dir.glob('/home/jack/programming/**/.git').select {|f| File.directory? f}
+require 'date'
+
+def print_date(y)
+  puts "--date=\"" + y.to_datetime.next_day().iso8601 + "\""
+end
+
+location = File.join(ARGV[0], '/**/.git')
+places = Dir.glob(location).select {|f| File.directory? f}
 times = []
 places.each { |place|
   x = `git --git-dir #{place} log -1 --pretty=format:"%at"`
   t = Time.at(x.to_i)
   times.push(t)
 }
-times.sort().collect {|x| puts x.to_s}
+x = times.sort().last
+offset = rand(180*60) - (60*60)
+y = x + offset
+if y.day == x.day then
+  print_date(y)
+else 
+  y = x - offset
+  print_date(y)
+end
 
