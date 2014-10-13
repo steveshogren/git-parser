bc23e266e9426935bbd76b4d76d4e2052559c746
Steve Shogren <steve.a.shogren@gmail.com>
2014-10-13 07:11:05 -0400

    Refactoring the git date stat gathering file

--- get-date.rb
+++ get-date.rb
@@ -3,6 +3,15 @@ require 'date'
 def print_date(y)
   puts "--date=\"" + y.to_datetime.next_day().iso8601 + "\""
 end
+def offsetDate(d)
+  offset = rand(180*60) - (60*60)
+  y = d + offset
+  if y.day != d.day then
+    y = d - offset
+  end
+  return y
+end
+
 place = ARGV[0] || "/home/jack/programming/"
 location = File.join(place, '/**/.git')
 places = Dir.glob(location).select {|f| File.directory? f}
@@ -13,12 +22,5 @@ places.each { |place|
   times.push(t)
 }
 x = times.sort().last
-offset = rand(180*60) - (60*60)
-y = x + offset
-if y.day == x.day then
-  print_date(y)
-else 
-  y = x - offset
-  print_date(y)
-end
 
+print_date(offsetDate(x))
